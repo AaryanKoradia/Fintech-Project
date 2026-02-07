bcf0e03d3bcab72a2305f9fbc62287c4f52e22aa
Aethra-Comp <princemaurya8879@gmail.com>
2026-02-07 05:58:28 +0530

    Add numbered primary color variants to Tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,9 +8,18 @@ export default {
   theme: {
     extend: {
       colors: {
-        // Warm Civic Design - Rural Digital Governance
         primary: {
-          DEFAULT: '#C2410C',  // Earthy Orange
+          50: '#FFF7ED',
+          100: '#FFEDD5',
+          200: '#FED7AA',
+          300: '#FDBA74',
+          400: '#FB923C',
+          500: '#F97316',
+          600: '#EA580C',
+          700: '#C2410C',
+          800: '#9A3412',
+          900: '#7C2D12',
+          DEFAULT: '#C2410C',
           hover: '#9A3412',
           light: '#FFF7ED',
         },
